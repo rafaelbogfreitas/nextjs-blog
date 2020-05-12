699027468c6f52fbfca836a0ef90eefdb36f5256
Rafael Freitas <rafaelborges1985@hotmail.com>
2020-05-12 18:03:14 -0300

    Finish next-js blog

---
+++ pages/posts/[id].js
@@ -0,0 +1,43 @@
+import Layout from '../../components/layout';
+import Date from '../../components/date'
+import Head from 'next/head';
+import { getAllPostIds, getPostData } from '../../lib/posts'
+import utilStyles from '../../styles/utils.module.css'
+
+export default function Post({ postData }) {
+  return (
+    <Layout>
+      <Head>
+        <title>{postData.title}</title>
+      </Head>
+      <article>
+        <h1 className={utilStyles.headingXl}>{postData.title}</h1>
+        <div className={utilStyles.lightText}>
+          <Date dateString={postData.date} />
+        </div>
+        <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
+      </article>
+    </Layout>
+  )
+}
+
+export async function getStaticPaths() {
+  // Return a list of possible value for id
+  const paths = getAllPostIds() //from lib
+  return {
+    paths,
+    fallback: false
+  }
+}
+
+
+// Fetch necessary data for the blog post using params.id
+export async function getStaticProps({ params }) {
+  console.log(params);
+  const postData = await getPostData(params.id)
+  return {
+    props: {
+      postData
+    }
+  }
+}
